c19f5b323aac42abff002b0775719e9c18f67ec1
Lê Duy Lâm <51469563+leduylam@users.noreply.github.com>
2021-06-10 23:30:44 +0700

    Add database and view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('/admin')->namespace('Admin')->group(function(){
+    Route::get('/dashboard', [AdminController::class, 'dashboard']);
 });
