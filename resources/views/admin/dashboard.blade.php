7a7d559a9c698876699773d68516793ae091eaa8
Lê Duy Lâm <51469563+leduylam@users.noreply.github.com>
2021-06-13 00:04:19 +0700

    Add new

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -50,7 +50,7 @@
                 <h4 class="mb-0">
                     <span class="count">10468</span>
                 </h4>
-                <p class="text-light">All Products</p>
+                <p class="text-light">Customers view</p>
 
                 <div class="chart-wrapper px-0" style="height:70px;" height="70">
                     <canvas id="widgetChart1"></canvas>
@@ -148,82 +148,6 @@
         </div>
     </div>
     <!--/.col-->
-
-    <div class="col-lg-3 col-md-6">
-        <div class="social-box facebook">
-            <i class="fa fa-facebook"></i>
-            <ul>
-                <li>
-                    <span class="count">40</span> k
-                    <span>friends</span>
-                </li>
-                <li>
-                    <span class="count">450</span>
-                    <span>feeds</span>
-                </li>
-            </ul>
-        </div>
-        <!--/social-box-->
-    </div>
-    <!--/.col-->
-
-
-    <div class="col-lg-3 col-md-6">
-        <div class="social-box twitter">
-            <i class="fa fa-twitter"></i>
-            <ul>
-                <li>
-                    <span class="count">30</span> k
-                    <span>friends</span>
-                </li>
-                <li>
-                    <span class="count">450</span>
-                    <span>tweets</span>
-                </li>
-            </ul>
-        </div>
-        <!--/social-box-->
-    </div>
-    <!--/.col-->
-
-
-    <div class="col-lg-3 col-md-6">
-        <div class="social-box linkedin">
-            <i class="fa fa-linkedin"></i>
-            <ul>
-                <li>
-                    <span class="count">40</span> +
-                    <span>contacts</span>
-                </li>
-                <li>
-                    <span class="count">250</span>
-                    <span>feeds</span>
-                </li>
-            </ul>
-        </div>
-        <!--/social-box-->
-    </div>
-    <!--/.col-->
-
-
-    <div class="col-lg-3 col-md-6">
-        <div class="social-box google-plus">
-            <i class="fa fa-google-plus"></i>
-            <ul>
-                <li>
-                    <span class="count">94</span> k
-                    <span>followers</span>
-                </li>
-                <li>
-                    <span class="count">92</span>
-                    <span>circles</span>
-                </li>
-            </ul>
-        </div>
-        <!--/social-box-->
-    </div>
-    <!--/.col-->
-
     <div class="col-xl-6">
         <div class="card">
             <div class="card-body">
@@ -301,62 +225,15 @@
         </div>
     </div>
 
-    <div class="col-xl-3 col-lg-6">
-        <section class="card">
-            <div class="twt-feed blue-bg">
-                <div class="corner-ribon black-ribon">
-                    <i class="fa fa-twitter"></i>
-                </div>
-                <div class="fa fa-twitter wtt-mark"></div>
+    
 
-                <div class="media">
-                    <a href="#">
-                        <img class="align-self-center rounded-circle mr-3" style="width:85px; height:85px;" alt="" src="images/admin.jpg">
-                    </a>
-                    <div class="media-body">
-                        <h2 class="text-white display-6">Jim Doe</h2>
-                        <p class="text-light">Project Manager</p>
-                    </div>
-                </div>
-            </div>
-            <div class="weather-category twt-category">
-                <ul>
-                    <li class="active">
-                        <h5>750</h5>
-                        Tweets
-                    </li>
-                    <li>
-                        <h5>865</h5>
-                        Following
-                    </li>
-                    <li>
-                        <h5>3645</h5>
-                        Followers
-                    </li>
-                </ul>
-            </div>
-            <div class="twt-write col-sm-12">
-                <textarea placeholder="Write your Tweet and Enter" rows="1" class="form-control t-text-area"></textarea>
-            </div>
-            <footer class="twt-footer">
-                <a href="#"><i class="fa fa-camera"></i></a>
-                <a href="#"><i class="fa fa-map-marker"></i></a>
-                New Castle, UK
-                <span class="pull-right">
-                    32
-                </span>
-            </footer>
-        </section>
-    </div>
-
-
-    <div class="col-xl-3 col-lg-6">
+    <div class="col-xl-6 col-lg-6">
         <div class="card">
             <div class="card-body">
                 <div class="stat-widget-one">
                     <div class="stat-icon dib"><i class="ti-money text-success border-success"></i></div>
                     <div class="stat-content dib">
-                        <div class="stat-text">Total Profit</div>
+                        <div class="stat-text">Total Price</div>
                         <div class="stat-digit">1,012</div>
                     </div>
                 </div>
@@ -371,7 +248,7 @@
                 <div class="stat-widget-one">
                     <div class="stat-icon dib"><i class="ti-user text-primary border-primary"></i></div>
                     <div class="stat-content dib">
-                        <div class="stat-text">New Customer</div>
+                        <div class="stat-text">Total Order</div>
                         <div class="stat-digit">961</div>
                     </div>
                 </div>
@@ -385,7 +262,7 @@
                 <div class="stat-widget-one">
                     <div class="stat-icon dib"><i class="ti-layout-grid2 text-warning border-warning"></i></div>
                     <div class="stat-content dib">
-                        <div class="stat-text">Active Projects</div>
+                        <div class="stat-text">Total Products</div>
                         <div class="stat-digit">770</div>
                     </div>
                 </div>
