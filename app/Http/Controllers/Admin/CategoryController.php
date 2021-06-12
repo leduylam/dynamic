74fc24f680002668884da9fcba0237e94f07072d
lant <lant@newwave.vn>
2021-06-12 23:24:22 +0700

    change sku admins, category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -32,7 +32,7 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        dd($request->all());
     }
 
     /**
--- routes/backend/admin.php
+++ routes/backend/admin.php
@@ -30,6 +30,7 @@ Route::group([
     ], function () {
         Route::get('/', [CategoryController::class, 'index'])->name('index');
         Route::get('/create', [CategoryController::class, 'create'])->name('create');
+        Route::post('/', [CategoryController::class, 'store'])->name('store');
     });
 });
 ?>
